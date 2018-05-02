244e5f0aec9dae60f3311106fb04933784d72d42
Rangel Reale <rangelspam@gmail.com>
2018-05-02 16:39:36 -0300

    godoc

---
+++ doc.go
@@ -0,0 +1,33 @@
+package refdump
+
+/*
+This library contains helpers to generate textual strings of the "reflect"
+package's values, like "reflect.Value", "reflect.Type", "reflect.Kind".
+
+    type S1 struct {
+        A1 int
+        A2 string
+    }
+
+    ta := &S1{
+        A1: 10,
+        A2: "Value",
+    }
+
+    fmt.Printf("%s\n" RefDumpValue(reflect.ValueOf(ta)))
+
+    type XX struct {
+    }
+
+    m := make(map[string]*XX)
+
+    fmt.Printf("%s\n", RefDumpValue(reflect.ValueOf(m)))
+
+
+Output:
+
+    Kind:(Ptr Struct) Name:(*main.S1) [!CanAddr,!CanSet]
+
+    Kind:(Map) Key:{Kind:(String)} Elem:{Kind:(Ptr Struct) Name:(*main.XX)} Len:(0) [!CanAddr,!CanSet]
+
+*/
